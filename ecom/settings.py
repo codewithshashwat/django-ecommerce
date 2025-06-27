e17d775740875bff49c3a01ac3a9ed2d0f0a929e
codewithshashwat <pritishshashwat@gmail.com>
2025-06-27 12:16:31 +0530

    Added bootstrap template

--- ecom/settings.py
+++ ecom/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATICFILES_DIRS = ['static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
